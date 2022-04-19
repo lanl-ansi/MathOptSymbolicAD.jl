7682950829e74e01213d315ca037a1e37d6b6459
odow <o.dowson@gmail.com>
2022-04-19 13:06:18 +1200

    Updates

--- src/nonlinear_oracle.jl
+++ src/nonlinear_oracle.jl
@@ -165,31 +165,26 @@ Parse `expr` into `f` and return a `_Node`.
     This function gets called recursively.
 """
 function _Node(f::_Function, expr::Expr)
-    if isexpr(expr, :call)
-        # Performance optimization: most calls will be unary or binary
-        # operators. Therefore, we can specialize an if-statement to handle the
-        # common cases without needing to splat.
-        if length(expr.args) == 2
-            return _Node(f, expr.args[1], _Node(f, expr.args[2]))
-        elseif length(expr.args) == 3
-            # Special cased performance optimization for _OP_INTEGER_EXPONENT
-            if expr.args[1] == :^ && _is_integer(expr.args[3])
-                return _Node(f, ^, _Node(f, expr.args[2]), Int(expr.args[3]))
-            end
-            return _Node(
-                f,
-                expr.args[1],
-                _Node(f, expr.args[2]),
-                _Node(f, expr.args[3]),
-            )
-        else
-            nodes = (_Node(f, expr.args[i]) for i in 2:length(expr.args))
-            return _Node(f, expr.args[1], nodes...)
+    @assert isexpr(expr, :call)
+    # Performance optimization: most calls will be unary or binary
+    # operators. Therefore, we can specialize an if-statement to handle the
+    # common cases without needing to splat.
+    if length(expr.args) == 2
+        return _Node(f, expr.args[1], _Node(f, expr.args[2]))
+    elseif length(expr.args) == 3
+        # Special cased performance optimization for _OP_INTEGER_EXPONENT
+        if expr.args[1] == :^ && _is_integer(expr.args[3])
+            return _Node(f, ^, _Node(f, expr.args[2]), Int(expr.args[3]))
         end
+        return _Node(
+            f,
+            expr.args[1],
+            _Node(f, expr.args[2]),
+            _Node(f, expr.args[3]),
+        )
     else
-        @assert isexpr(expr, :ref)
-        @assert expr.args[1] == :x
-        return _Node(f, expr.args[2])
+        nodes = (_Node(f, expr.args[i]) for i in 2:length(expr.args))
+        return _Node(f, expr.args[1], nodes...)
     end
 end
 
