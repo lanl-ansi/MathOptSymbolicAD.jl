88da754fb3953c0a71a96f453d6c8ff2ed3bc1c7
Oscar Dowson <odow@users.noreply.github.com>
2022-08-17 10:51:29 +1200

    Add license headers (#18)

--- src/ThreadedBackend.jl
+++ src/ThreadedBackend.jl
@@ -1,6 +1,7 @@
-###
-### ThreadedBackend
-###
+# Copyright (c) 2022: Oscar Dowson, and contributors
+#
+# Use of this source code is governed by an MIT-style license that can be found
+# in the LICENSE.md file or at https://opensource.org/licenses/MIT.
 
 struct _ConstraintOffset
     index::Int
