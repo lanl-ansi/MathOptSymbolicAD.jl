955433b619af1280e5ac840e3be4e4920e5d1fd1
Michel Schanen <mschanen@anl.gov>
2023-01-12 15:30:25 -0600

    Fix subexpressions in expressions

--- src/nonlinear_oracle.jl
+++ src/nonlinear_oracle.jl
@@ -536,8 +536,9 @@ function MOI.Nonlinear.Evaluator(
 )
     variable_order =
         Dict(x.value => i for (i, x) in enumerate(ordered_variables))
-    subexpressions = map(model.expressions) do expr
-        return _to_expr(model, expr, variable_order, Expr[])::Expr
+    subexpressions = Vector{Expr}()
+    for sub in model.expressions
+        push!(subexpressions, _to_expr(model, sub, variable_order, subexpressions))
     end
     objective = nothing
     if model.objective !== nothing
